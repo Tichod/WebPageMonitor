5cf84e9e394962e9e04e4053e22cd6728bc210a1
Tichod <18058781+Tichod@users.noreply.github.com>
2022-11-09 17:14:24 +0100

    Update WebpageMonitor.sh
    Minor edits

--- WebpageMonitor.sh
+++ WebpageMonitor.sh
@@ -2,23 +2,27 @@
 # WebPageMonitor.sh
 # by Tichod <https://github.com/Tichod>
 # Created   : <13/12/2012 11:14:51 Tichod>
-# Last modif: <31/01/2021 19:44:52 Tichod>
+# Last modif: <09/11/2022 17:11:12 Tichod>
 
 # ================== Licence                   BEGIN ==================
            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
                    Version 2, December 2004
- 
+
 Copyright (C) 2021 Tichod <https://github.com/Tichod>
 
 Everyone is permitted to copy and distribute verbatim or modified
 copies of this license document, and changing it is allowed as long
 as the name is changed.
- 
+
            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
  0. You just DO WHAT THE FUCK YOU WANT TO.
 # ================== Licence                    END   ==================
+
+# Ran by: crontab
+## Web Page Monitoring
+#56 * * * *  /bin/bash ~/bin/WebPageMonitor.sh
 
 # Description : WebPageMonitor.sh checks if web pages have been modified and sends a notification by email if so
 
@@ -28,20 +32,21 @@ RootDir=~/veille/
 SourceServer=$(hostname --fqdn)
 export URLList=${RootDir}/url_list
 # File format:
-# National Cybersecurity Center of Excellence - News;https://www.nccoe.nist.gov/news;NCCOE-News;theme_token\|form_build_id\|view-dom-id\|views-field
-# \__________________________________________/ \___________________________________/ \________/ \__________________________________________________/
-#                      \                                          \                       \                 \
-#                       \                                          \                       \                 \-> cf. [1] hereunder
-#                        \                                          \                       \-> storage directory (under ${RootDir})
-#                         \                                          \
-#                          \                                          \-> webpage URL
-#                           \-> title (present in subject of the notification email
+# National Cybersecurity Center of Excellence - News;https://www.nccoe.nist.gov/news;NCCOE-News;xxx.xxxx@example.org;theme_token\|form_build_id\|view-dom-id\|views-field
+# \________________________________________________/ \_____________________________/ \________/ \__________________/ \____________________________________________________/
+#                     \                                           \                       \               \                              \
+#                      \                                           \                       \               \                              \-> cf. [1] hereunder
+#                       \                                           \                       \               \-> recipient email address
+#                        \                                           \                       \-> storage directory (under ${RootDir})
+#                         \                                           \-> site URl
+#                          \-> title (present in subject of the notification email)
 # Notes :
 # [1] Some pages contain frequently changing code, which does not affect the visible content of the page.
 #     In order to disregard such changes, lines containing these character strings are excluded from the comparison.
 #
 #     /!\ chain separator =  << \| >>
-Recipient=tichod@example.org
+#     /!\ no space in character strings
+#     /!\ test before go-live
 DefaultEscapedString='generated|served|expires|<!--|-->|batcache|html|meta|googleanalytics|twitter|nonce'
 # ================== Variables definition       END  ==================
 
@@ -58,47 +63,49 @@ then
   do
       if [[ (! "$Ligne" =~ ^#.*) && (! "$Ligne" =~ ^$) ]]
       then
-	Title=$(echo $Ligne | gawk -F ";" '{print $1}')
-	RenamedTitle=$(echo $Title | sed -e 's/ /_/g')
-	URL=$(echo $Ligne | gawk -F ";" '{print $2}')
-	Dir=${RootDir}$(echo $Ligne | gawk -F ";" '{print $3}')/
-	EscapedString=$(echo $Ligne | gawk -F ";" '{print $4}')
-	MessageBody=${Dir}message
-	if [[ ! -e $Dir ]]
-	then
-	  mkdir $Dir
-	fi # End of : if [[ ! -e $Dir ]]
+        Title=$(echo $Ligne | gawk -F ";" '{print $1}')
+        RenamedTitle=$(echo $Title | sed -e 's/ /_/g')
+        URL=$(echo $Ligne | gawk -F ";" '{print $2}')
+        Dir=${RootDir}$(echo $Ligne | gawk -F ";" '{print $3}')/
+        Recipient=$(echo $Line | gawk -F ";" '{print $4}')
+        EscapedString=$(echo $Line | gawk -F ";" '{print $4}')
+        MessageBody=${Dir}message
+        if [[ ! -e $Dir ]]
+        then
+          mkdir $Dir
+        fi # End of : if [[ ! -e $Dir ]]
 
-	mv -f ${Dir}$RenamedTitle ${Dir}${RenamedTitle}.previous
-	wget -q --no-check-certificate $URL -O ${Dir}$RenamedTitle
-	
-	# Suppression of lines containing strings without affecting the visible content of the page
-	if [[ X"$EscapedString" == "X" ]]
-	then
-	  grep -viE "$DefaultEscapedString" ${Dir}$RenamedTitle > "$0.tmp" && mv -f "$0.tmp" ${Dir}$RenamedTitle
-	else
-	    grep -viE "$DefaultEscapedString|$EscapedString" ${Dir}$RenamedTitle > "$0.tmp" && mv -f "$0.tmp" ${Dir}$RenamedTitle
-	fi # End of : if [[ X"$EscapedString" == "X" ]]
-	diff -q ${Dir}${RenamedTitle}.previous ${Dir}$RenamedTitle >>/dev/null 2>&1
-	DiffResult="$?"
+        mv -f ${Dir}$RenamedTitle ${Dir}${RenamedTitle}.previous
+        wget -q --no-check-certificate $URL -O ${Dir}$RenamedTitle
 
-	if [[ "$DiffResult" -eq 0 ]]
-	then
-	  true
-	else
-	    Subject="WebPageMonitor - New information published by $Title"
-	    echo "URL : $URL" > $MessageBody
-	    echo "Difference :" >> $MessageBody
-	    diff  ${Dir}${RenamedTitle}.previous ${Dir}$RenamedTitle >> $MessageBody
-	    echo "================" >> $MessageBody
-	    Signature >> $MessageBody
-	    cat $MessageBody | mail -s "$Subject" "$Recipient"
-	fi # End of : if [[ "$DiffResult" -eq 0 ]]
+        # Suppression of lines containing frequently changing strings without affecting the visible content of the page
+        if [[ X"$EscapedString" == "X" ]]
+        then
+          grep -viE "$DefaultEscapedString" ${Dir}$RenamedTitle > "$0.tmp" && mv -f "$0.tmp" ${Dir}$RenamedTitle
+        else
+            grep -viE "$DefaultEscapedString|$EscapedString" ${Dir}$RenamedTitle > "$0.tmp" && mv -f "$0.tmp" ${Dir}$RenamedTitle
+        fi # End of : if [[ X"$EscapedString" == "X" ]]
+        diff -q ${Dir}${RenamedTitle}.previous ${Dir}$RenamedTitle >>/dev/null 2>&1
+        DiffResult="$?"
+
+        if [[ "$DiffResult" -eq 0 ]]
+        then
+          true
+          # Possibility: send an email regularly to check if script running correctly (false negative)
+        else
+            Subject="WebPageMonitor - New information published by $Title"
+            echo "URL : $URL" > $MessageBody
+            echo "Difference :" >> $MessageBody
+            diff  ${Dir}${RenamedTitle}.previous ${Dir}$RenamedTitle >> $MessageBody
+            echo "================" >> $MessageBody
+            Signature >> $MessageBody
+            cat $MessageBody | mail -s "$Subject" "$Recipient"
+        fi # End of : if [[ "$DiffResult" -eq 0 ]]
       else
-	  # Commented out text or empty line
-	  true
-      fi # End of : if [[ ! "$Ligne" =~ "^#" ]]
-  done  < $URLList # End of : while read Ligne
+          # Commented out text or empty line
+          true
+      fi # End of : if [[ ! "$Line" =~ "^#" ]]
+  done  < $URLList # End of : while read Line
 else
     echo "Can't find configuration file $URLList, create it" | mail -s "Incident $0" $Recipient
     exit 1
